3ce6a525767d6b83a6eb33e45a834b6e5e74e762
Ivar <lamnt6893@gmail.com>
2023-09-06 21:46:30 +0700

    404 routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
 	// {path: '404', component: NotfoundComponent, data: {animation: 'Restrict'}},
 	// {path: '500', component: MaintainedComponent, data: {animation: 'Restrict'}},
 	{path: '', redirectTo: '/trang-chu', pathMatch: 'full'},
-	// {path: '**', redirectTo: '/404'}, // must be the end of this list
+	{path: '**', redirectTo: '/404'}, // must be the end of this list
 ];
 
 @NgModule({
